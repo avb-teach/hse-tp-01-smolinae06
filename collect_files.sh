47c6d2481bdd954add68fd3e7eef4cce3b766d00
Елизавета Смолина <elizavetasmolina@Noutbukizaveta2.domrurouter.net>
2025-04-27 16:24:52 +0300

    start6

--- collect_files.sh
+++ collect_files.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-# Проверяем, что передано ровно два параметра. В противном случае, выводим сообщение об ошибке и корректном использовании скрипта и завершаем его выполнение
 if (( $# != 2 )); then
     echo "Ошибка: требуется два параметра; Корректное использование скрипта: $0 /path/to/input_dir /path/to/output_dir"
     exit 1 
 fi
 
-# Присваивание переменных, которые будут использоваться для указания входной и выходной директорий
 input_dir="$1"
 output_dir="$2"
 
-# Проверяем, существует ли входная директория. В противном случае, выводим ошибку и заверашем выполнение скрипта
 if test ! -d "$input_dir"; then
     echo "Ошибка: входная директория "$input_dir" не существует"
     exit 1
 fi
 
-# Создаём выходную директорию, если она не существует
-if test ! -d "$output_dir"; then
-   mkdir "$output_dir"
-fi
+files_copying() {
+    local from_dir="$1"
+    local to_dir="$2"
+    for element in "$from_dir"/*; do
+        if test -f "element"; then
+            local name=$(basename "$element")
+            local to_file="$to_dir/$name"
+            local k=1
+            while test -e "$to_file"; do
+                to_file="$(
+                    echo "${to_dir}/"
+                    echo "${name%.*}.$k."
+                    echo "${name##*.}"
+                )"
+                ((++k))
+            done
+            cp "$element" "$to_file"
+            
+        elif test -d "$element"; then
+            mkdir -p "$to_dir/$(basename "$element")"
+            files_copying "$element" "$to_dir/$(basename "$element")"
+        fi
+    done
+}
 
-rsync -r "$input_dir/" "$output_dir/"
+files_copying "$input_dir" "$output_dir"
 echo "Все файлы скопированы из "$input_dir" в "$output_dir""
