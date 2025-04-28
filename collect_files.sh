758e4e3216de8c62a91990f347730587055fa5dd
Елизавета Смолина <elizavetasmolina@Noutbukizaveta2.domrurouter.net>
2025-04-28 15:27:17 +0300

    start0

--- collect_files.sh
+++ collect_files.sh
@@ -7,18 +7,6 @@ parametrs_check() {
     fi
 }
 
-unique_name() {
-    local k=1
-    local cur_name="$1"
-    local dir="$2"
-    local new_name="$cur_name"
-    while test -e "$dir/$new_name"; do
-                new_name="${cur_name%.*}.$k.${cur_name##*.}"
-                ((k+=1))
-            done
-            echo "$new_name"
-}
-
 files_copying() {
     local from_dir="$1"
     local to_dir="$2"
@@ -26,9 +14,17 @@ files_copying() {
         local name="${element##*/}"
         local to_file="$to_dir/$name"
         if test -f "$element"; then
-            to_file=$(unique_name "$name" "$to_dir")
+            local k=1
+            while test -e "$to_file"; do
+                to_file="$(
+                    echo "${to_dir}/"
+                    echo "${name%.*}.$k."
+                    echo "${name##*.}"
+                )"
+                ((k+=1))
+            done
             cp "$element" "$to_file"
-           
+
         elif test -d "$element"; then
             mkdir -p "$to_file"
             files_copying "$element" "$to_file"
